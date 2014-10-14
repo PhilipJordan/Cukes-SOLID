ea21bcca41548c598ccd341cc12035b83abd9de6
Philip Jordan <philip.jordan@live.com>
2014-10-14 10:43:51 -0400

    cleaning up existing cukes

---
+++ Cukes/features/support/hooks.rb
@@ -0,0 +1,16 @@
+require 'watir-webdriver'
+
+browser = Watir::Browser.new :firefox #:chrome, :switches => %w[--test-type]# 
+
+Before do
+	@browser = browser #Watir::Browser.new :firefox# 
+end
+
+After do
+	on(MissionPage).reset
+	#browser.goto "#{$environment}" #/Mission/Index" 
+end
+
+at_exit do
+	browser.close
+end
